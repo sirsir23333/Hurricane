8c6cd96b97919961a9a761a36735e9a9ff63730a
neoxiuyang <neoxiuyang@gmail.com>
2024-10-08 13:42:35 +0800

    Automated commit 2024-10-08 13:42:35

---
+++ auto_upload.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Update the path to bash.exe based on where Git Bash is installed
+"C:\Path\To\Git\bin\bash.exe" -c "/c/Users/CrudeIntern/OneDrive\ -\ Hengli\ Petrochemical\ International\ Pte\ Ltd/Market\ Analysis/Current\ Projects/Hurricane/auto_upload.sh"
